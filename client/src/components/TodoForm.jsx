fe5fd7c4910d1d9ba8f0c2d68b839673dd1b729d
Piyush Keshri <piyushkeshri383@gmail.com>
2023-02-25 16:37:36 +0530

    Initital Setup

---
+++ client/src/components/TodoForm.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+
+const TodoForm = () => {
+    const [text, setText] = useState("");
+
+    const onFormSubmit = () => {
+
+    }
+    const onInputChange = (e) => {
+        setText(e.target.value);
+    }
+
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+            <input
+                placeholder="Enter Your Task..."
+                className="input"
+                onChange={onInputChange}
+            />
+        </form>)
+
+}
+
+export default TodoForm;
